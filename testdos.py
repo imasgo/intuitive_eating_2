049be89e0e83b5a5d3d368ab82f07ac78b641d4a
Imasgo <sasmashka@gmail.com>
2019-08-04 20:00:00 -0600

    first commit

--- testdos.py
+++ testdos.py
@@ -1,3 +1,3 @@
 print("test numero dos tres quatro bep bep bep bep beeeep bep bep")
 print("test numero dos tres quatro bep bep bep bep beeeep bep bep")
-print("test numero dos tres quatro bep bep bep bep beeeep bep bep be be bep")
+print("test numero dos tres quatro bep bep bep bep beeeep bep bep be be bep bep")
