2cdeb4384e233226698e3e445dee6de5936e5d8b
Imasgo <sasmashka@gmail.com>
2019-09-01 20:00:00 -0600

    first commit

--- testdos.py
+++ testdos.py
@@ -9,3 +9,5 @@ print("test numero dos tres quatro bep bep bep bep beeeep bep bep be be bep bep 
 print("test numero dos tres quatro bep bep bep bep beeeep bep bep be be bep bep bep bep bep bep bep bep")
 print("oh how much I want to work")
 print("I am the best employee")
+print("Not only a pm but also a developer")
+
