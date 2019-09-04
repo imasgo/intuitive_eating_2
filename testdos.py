2fea21dc6a1477117bc9b2e7c681585198812580
Imasgo <sasmashka@gmail.com>
2019-09-03 20:00:00 -0600

    first commit

--- testdos.py
+++ testdos.py
@@ -11,5 +11,7 @@ print("oh how much I want to work")
 print("I am the best employee")
 print("Not only a pm but also a developer")
 print("This is for everyone saying I lack technical experience")
+print("You might be right but how many other people you know who did this")
+
 
 
